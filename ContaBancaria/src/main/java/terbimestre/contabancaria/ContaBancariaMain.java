c6aabc0a6cbdca8f33ac0657cee92d3bd442a482
Alan Henrique <alanhipermassa@gmail.com>
2023-05-17 19:58:22 -0300

    Exercício 2

--- ContaBancaria/src/main/java/terbimestre/contabancaria/ContaBancariaMain.java
+++ ContaBancaria/src/main/java/terbimestre/contabancaria/ContaBancariaMain.java
@@ -1,3 +1,8 @@
+/**
+ * 2 - Crie uma classe chamada ContaBancaria com atributos privados como saldo e titular. Utilize encapsulamento para permitir encapsulamento 
+ * para permitir o acesso a esses atributos por meio de métodos getters e setters
+ */
+
 package terbimestre.contabancaria;
 
 /**
