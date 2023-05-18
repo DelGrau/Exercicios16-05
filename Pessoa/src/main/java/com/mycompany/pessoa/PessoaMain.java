5e31d758326c78bd0f31e4fba0b22bdd53919107
adrianlucastdk <101604988+adrianlucastdk@users.noreply.github.com>
2023-05-17 22:11:15 -0300

    Exercício 6

--- Pessoa/src/main/java/com/mycompany/pessoa/PessoaMain.java
+++ Pessoa/src/main/java/com/mycompany/pessoa/PessoaMain.java
@@ -1,6 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+/* 
+* Crie uma classe chamada Pessoa com atributos privados como "nome" e "idade". Utilize encapsulamento para permitir o acesso a esses atributos por meio de métodos getters e setters
+*
+*/
 
 package com.mycompany.pessoa;
 
