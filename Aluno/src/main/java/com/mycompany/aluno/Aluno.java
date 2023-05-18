7f575f98f05d79071367c7f7a124e79e7e6c76b6
adrianlucastdk <101604988+adrianlucastdk@users.noreply.github.com>
2023-05-17 22:20:39 -0300

    Exercício 7

---
+++ Aluno/src/main/java/com/mycompany/aluno/Aluno.java
@@ -0,0 +1,17 @@
+/*
+* Crie uma Classe cahamda  Aluno que herda da classe "Pessoa" e possui um atributo privado "matricula". Utilize encapsulamento para permitir o acesso ao atributo
+* "matricula" por meio de métodos getters e setters.
+*/
+
+package com.mycompany.aluno;
+
+/**
+ *
+ * @author Adrian
+ */
+public class Aluno extends Pessoa{
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Aluno/src/main/java/com/mycompany/aluno/Pessoa.java
@@ -0,0 +1,21 @@
+
+package com.mycompany.aluno;
+
+/**
+ *
+ * @author Adrian
+ */
+public class Pessoa {
+    private String Matricula;
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    public String getMatricula() {
+        return this.Matricula;
+    }
+    public void setMatricula(String pMatricula) {
+        this.Matricula = pMatricula;
+    }
+}
